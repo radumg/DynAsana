9682c1794e650b1efb826533ef80b3322c33a488
Radu Gidei <radugidei@gmail.com>
2017-05-07 19:44:33 +0100

    remove Slack settings
    Slack settings were part of the original DynaSlack that DynAsana was
    forked from.

--- src/DynAsana/Properties/Settings.Designer.cs
+++ src/DynAsana/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace Asana.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.1.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -20,54 +20,6 @@ namespace Asana.Properties {
         public static Settings Default {
             get {
                 return defaultInstance;
-            }
-        }
-        
-        [global::System.Configuration.UserScopedSettingAttribute()]
-        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("")]
-        public string slackAuthToken {
-            get {
-                return ((string)(this["slackAuthToken"]));
-            }
-            set {
-                this["slackAuthToken"] = value;
-            }
-        }
-        
-        [global::System.Configuration.UserScopedSettingAttribute()]
-        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("")]
-        public string slackBotToken {
-            get {
-                return ((string)(this["slackBotToken"]));
-            }
-            set {
-                this["slackBotToken"] = value;
-            }
-        }
-        
-        [global::System.Configuration.UserScopedSettingAttribute()]
-        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("")]
-        public string slackWebhookUrl {
-            get {
-                return ((string)(this["slackWebhookUrl"]));
-            }
-            set {
-                this["slackWebhookUrl"] = value;
-            }
-        }
-        
-        [global::System.Configuration.UserScopedSettingAttribute()]
-        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("")]
-        public string slackWebhookChannel {
-            get {
-                return ((string)(this["slackWebhookChannel"]));
-            }
-            set {
-                this["slackWebhookChannel"] = value;
             }
         }
     }
